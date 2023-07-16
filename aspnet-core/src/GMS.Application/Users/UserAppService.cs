a45802b340d977f9056bb88827ab364a125cf3cd
tguankheng016 <102196120+tguankheng016@users.noreply.github.com>
2023-07-16 08:58:58 +0800

    Update UserAppService.cs

--- aspnet-core/src/GMS.Application/Users/UserAppService.cs
+++ aspnet-core/src/GMS.Application/Users/UserAppService.cs
@@ -55,7 +55,7 @@ namespace GMS.Users
 
         public override async Task<UserDto> CreateAsync(CreateUserDto input)
         {
-            var test = false;
+            var test = true;
             CheckCreatePermission();
 
             var user = ObjectMapper.Map<User>(input);
